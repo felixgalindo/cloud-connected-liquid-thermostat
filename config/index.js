cdedab38f648fee7fa2da1a48c995587d1e9b6ee
Felix Galindo <felixgalindo91@gmail.com>
2016-04-09 01:22:12 -0500

    Added comments.

--- config/index.js
+++ config/index.js
@@ -1,15 +1,17 @@
-//Options
-//Modify to your liking
+/**
+ * Definition of liquidStat options object
+ * @author Felix Galindo
+ */
 
 const minute = 60 * 1000;
 const options = {
-	tempSetPoint: 70,
-	graphingEnabled: 1,
-	sampleRate: minute/4, //sample rate
-	graphRate: minute, //graphing rate
-	relayGpioNum: 29, //Gpio # where heater control relay is wired
-	plotlyUsername: 'felixgalindo91',
-	plotlyApiKey: '54wtcm6ncf',
+	tempSetPoint: 80, //Liquid temperature to maintain
+	graphingEnabled: true, //Set to false if you don't want to graph 
+	sampleRate: minute/2, //sample rate in mS
+	graphRate: minute, //graphing rate in mS
+	relayGpioNum: 29, //Gpio # where heater control relay is wired, switch to what yours is wired to
+	plotlyUsername: 'felixgalindo91', //https://plot.ly/settings/api   , username
+	plotlyApiKey: '54wtcm6ncf', //https://plot.ly/settings/api , apikey
 	relayOnDelay: 3 * minute, //Wait time before turning relay on again from the last time it was turned on
 	ds18b20Id: '28-80000003a557'
 };
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * Definition of liquidStat. liquidStat monitors liquid level, monitors liquid sensor,
+ * and regulates liquid temperature. Sensor data is pushed to plotly for graphing.
+ * @author Felix Galindo
+ */
+
+
 var liquidStat = this;
 liquidStat.options = require("./config/")();
 console.log("Cloud Connected Liquid Thermostat Starting with options: ", liquidStat.options);
@@ -5,30 +12,28 @@ liquidStat.ds18b20Helper = require('./lib/ds18b20Helper.js')();
 liquidStat.eTapeHelper = require('./lib/eTapeHelper.js')();
 liquidStat.plotlyHelper = require('./lib/plotlyHelper.js')();
 liquidStat.gpioHelper = require('./lib/gpioHelper.js')();
+liquidStat.counter = liquidStat.options.relayOnDelay; //Start heating at the beginning if temp is too cold
 
-//console.log("liquid stat",liquidStat);
-var counter = liquidStat.options.relayOnDelay; //Start heating at the beginning if temp is too cold
-
+//Reads sensor data on specified interval and controls heater 
 setInterval(function() {
 	if (liquidStat.ds18b20Helper.listOfDeviceIds) {
 		liquidStat.ds18b20Helper.getTemp(liquidStat.options.ds18b20Id)
 			.then(function(liquidTemp) {
 				liquidStat.liquidTemp = liquidTemp;
-				console.log("liquid temp is ", liquidStat.liquidTemp, "degrees F");
+				console.log("Liquid temp is ", liquidStat.liquidTemp, "°F");
 			})
 			.then(function() {
 				return liquidStat.eTapeHelper.getLiquidLevel();
 			})
 			.then(function(liquidLevel) {
 				liquidStat.liquidLevel = liquidLevel;
-				console.log("liquid Level is ", liquidStat.liquidLevel, "inches");
+				console.log("Liquid Level is ", liquidStat.liquidLevel, "inches");
 
-				//Only turn on relay after 3 minute delay if temp is too cold
-				if (liquidStat.liquidTemp !== false && liquidStat.liquidTemp < liquidStat.options.tempSetPoint
-				 && counter > (liquidStat.options.relayOnDelay / liquidStat.options.sampleRate)) {
-					counter = 0;
+				//Check to see if we need to heat or we need to stop heating
+				if (liquidStat.liquidTemp !== false && liquidStat.liquidTemp < liquidStat.options.tempSetPoint && liquidStat.counter > (liquidStat.options.relayOnDelay / liquidStat.options.sampleRate)) {
+					liquidStat.counter = 0;
 					console.log("Turning heater on ");
-					liquidStat.gpioHelper.setPinHigh(relayGpioNum);
+					liquidStat.gpioHelper.setPinHigh(liquidStat.options.relayGpioNum);
 					liquidStat.heaterStaus = "On";
 				} else if (liquidStat.liquidTemp !== false && liquidStat.liquidTemp > liquidStat.options.tempSetPoint) {
 					console.log("Turning heater off");
@@ -41,23 +46,29 @@ setInterval(function() {
 				console.log(err);
 			});
 	}
-	counter++;
+	liquidStat.counter++;
 
 }, liquidStat.options.sampleRate);
 
+//Pushes to plotly on specified interval
 if (liquidStat.options.graphingEnabled) {
 	setInterval(function() {
-		if (liquidStat.liquidLevel != null && liquidStat.liquidLevel != undefined) {
-			liquidStat.plotlyHelper.plotData("Water Level", liquidStat.liquidLevel);
+		if (liquidStat.liquidLevel !== null && liquidStat.liquidLevel !== undefined) {
+			liquidStat.plotlyHelper.plotData("Liquid Level", liquidStat.liquidLevel);
 		}
-		if (liquidStat.heaterStaus != null && liquidStat.heaterStaus != undefined) {
-			liquidStat.plotlyHelper.plotData("Water Heater Status", liquidStat.heaterStaus);
+		if (liquidStat.heaterStaus !== null && liquidStat.heaterStaus !== undefined) {
+			liquidStat.plotlyHelper.plotData("Liquid Heater Status", liquidStat.heaterStaus);
 		}
-		if (liquidStat.liquidTemp != null && liquidStat.liquidTemp != undefined) {
-			liquidStat.plotlyHelper.plotData("Water Temperature", liquidStat.liquidTemp);
+		if (liquidStat.liquidTemp !== null && liquidStat.liquidTemp !== undefined) {
+			liquidStat.plotlyHelper.plotData("Liquid Temperature", liquidStat.liquidTemp);
 		}
-		if (liquidStat.options.tempSetPoint != null && liquidStat.options.tempSetPoint != undefined) {
-			liquidStat.plotlyHelper.plotData("Water Temp Set Point", liquidStat.options.tempSetPoint);
+		if (liquidStat.options.tempSetPoint !== null && liquidStat.options.tempSetPoint !== undefined) {
+			liquidStat.plotlyHelper.plotData("Liquid Temp Set Point", liquidStat.options.tempSetPoint);
 		}
 	}, liquidStat.options.graphRate);
+
+	module.exports = function() {
+		var instance = new liquidStat();
+		return instance;
+	};
 }
